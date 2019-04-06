24d275f8d082dac2fc0aaa106da153c1244feece
youhyun97 <paris972006@gmail.com>
2019-04-07 05:01:52 +0900

    file upload & download

--- ds_operator/board/urls.py
+++ ds_operator/board/urls.py
@@ -1,10 +1,12 @@
+
 from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('<int:board_id>',views.detail, name="detail"),
-    path('post/',views.post, name='post'),
-    path('show/', views.show, name='show'),
+    path('<int:board_id>',views.detail, name="detail"), # 글 하나 보여주기
+    path('post/',views.boardpost, name='post'), # 글쓰기
+    path('show/', views.show, name='show'), # 전체 게시글
     #path('photo/', views.photo, name="photo"),
 ]
