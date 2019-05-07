4ccd5bfe93810e575be67fea5c1636fc302dfa20
youhyun97 <paris972006@gmail.com>
2019-05-07 21:20:35 +0900

    .pyc 삭제

--- ds_operator/board/models.py
+++ ds_operator/board/models.py
@@ -4,6 +4,15 @@ class Board(models.Model):
     file = models.FileField(null=True)
     title = models.CharField(max_length=10, blank=True) # 제목
     updated_at = models.DateTimeField(auto_now=True) # 현재시각
-
+    like_user_set = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                    blank=True,
+                                    related_name='like_user_set',
+                                    through='Like')
     def __str__(self):
         return self.title
+
+    def like_count(self):
+        return self.like_user_set.count()
+
+class Like(models.Model):
+    post = models.ForeignKey(Post)
