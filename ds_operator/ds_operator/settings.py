28c5e97dda0f7eaac4b09e67b8846618f3a72d5a
Beakjiyeon <bjy1530@naver.com>
2019-05-09 00:08:04 +0900

    stamp 앱 등록,세션설정,css파일

--- ds_operator/ds_operator/settings.py
+++ ds_operator/ds_operator/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'stamp.apps.StampConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
